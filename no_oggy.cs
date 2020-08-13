2deaee36bcda603cdca49b66d2462693352f5d3d
ApoorvaK1998 <66967956+ApoorvaK1998@users.noreply.github.com>
2020-08-13 16:43:11 +0530

    Update no_oggy.cs

--- no_oggy.cs
+++ no_oggy.cs
@@ -21,7 +21,7 @@ class NoOggyChecker
   static int Main()
   {
     var names = new List<string>
-      { "nobita", "bheem", "oggy", "oggy cockroach", "ninja" };
+      { "nobita", "bheem", "oggy", "oggy cockroach", "ninja" ,"tom" };
    
     RemoveOggy(names);
     if(OggyIsRemoved(names)) {
